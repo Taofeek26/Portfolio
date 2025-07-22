a594e2e8c6f004139cb0345c5411e919f9f5e7e6
Taofeek26 <akannibolaji.taofeek@gmail.com>
2025-07-22 14:10:15 +0100

    My Portfolio Website

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,3 +1,5 @@
+import { NavLink } from 'react-router-dom';
+
 const Home = () => {
   return (
     <div className="text-white min-h-screen flex items-center justify-center">
@@ -5,9 +7,9 @@ const Home = () => {
         <h1 className="text-5xl font-extrabold mb-4">Abdulrauf Taofeek Bolaji</h1>
         <p className="text-xl text-gray-200 mb-8">Data Analyst | Mechatronics Engineer | Python Developer</p>
         <div className="space-x-4">
-          <a href="/projects" className="bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-3 px-6 rounded-lg transition-transform transform hover:scale-105">
+          <NavLink to="/projects" className="bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-3 px-6 rounded-lg transition-transform transform hover:scale-105">
             View My Projects
-          </a>
+          </NavLink>
           <a href="/assets/ABDULRAUF TAOFEEK Bolaji.docx" className="bg-gray-700 hover:bg-gray-800 text-white font-bold py-3 px-6 rounded-lg transition-transform transform hover:scale-105" download>
             Download CV
           </a>
