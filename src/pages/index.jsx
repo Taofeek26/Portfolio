71bdfbe89cdff633b783d2c3fdf595a8e7079eb8
Taofeek26 <akannibolaji.taofeek@gmail.com>
2025-07-22 15:29:40 +0100

    My Portfolio Website

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,7 +5,7 @@ const Home = () => {
     <div className="text-white min-h-screen flex items-center justify-center">
       <div className="text-center">
         <h1 className="text-5xl font-extrabold mb-4">Abdulrauf Taofeek Bolaji</h1>
-        <p className="text-xl text-gray-200 mb-8">Data Analyst | Mechatronics Engineer | Python Developer</p>
+        <p className="text-xl text-gray-200 mb-8">Data Analyst | Mechanical Engineering | Mechatronics Engineer | Software Developer | </p>
         <div className="space-x-4">
           <NavLink to="/projects" className="bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-3 px-6 rounded-lg transition-transform transform hover:scale-105">
             View My Projects
